ad76dae6ec2f9d3e315fc95a0db16b5eddc7fb04
mokolodi1 <mokolodi1@gmail.com>
2015-08-07 15:44:13 -0700

    update to go with using template-level subscriptions

--- webapp/server/startup/before/generateCohortSignatures.js
+++ webapp/server/startup/before/generateCohortSignatures.js
@@ -10,51 +10,54 @@ generateCohortSignatures = function () {
       console.log(error);
     }
     //console.log("result: " + result);
-  }
+  };
 
-  Signatures.find({}, {sort: {"description": 1}}).forEach(function (currentSignature){
-    console.log("generating signature scores for " + currentSignature.description);
-    var newSignatureScore = {
-      "signature_id": currentSignature._id,
-      "type": currentSignature.type,
-      "algorithm": currentSignature.algorithm,
-      "label": currentSignature.label,
-      "description": currentSignature.description,
-      "sample_values": [],
-      "gene_label": currentSignature.gene_label,
-    };
-
-    var getPatientLabelFromSampleLabel = function (sampleLabel) {
-      var patient = Patients.findOne({
-        "samples": {
-          $elemMatch: {
-            "sample_label": sampleLabel
-          }
+  var getPatientLabelFromSampleLabel = function (sampleLabel) {
+    var patient = Patients.findOne({
+      "samples": {
+        $elemMatch: {
+          "sample_label": sampleLabel
         }
-      });
-
-      if (patient) {
-        return patient.patient_label;
       }
-      return undefined;
+    });
+
+    if (patient) {
+      return patient.patient_label;
     }
+    return undefined;
+  };
 
-    signature_scores_old.find({ "name": currentSignature.description })
-        .forEach(function (currentOldScore) {
-      newSignatureScore.sample_values.push({
-        "patient_label": getPatientLabelFromSampleLabel(currentOldScore.id),
-        "sample_label": currentOldScore.id,
-        "value": currentOldScore.val || currentOldScore.value,
+  Signatures.find({}, {sort: {"description": 1}})
+      .forEach(function (currentSignature) {
+        console.log("generating signature scores for " + currentSignature.description);
+        var newSignatureScore = {
+          "signature_id": currentSignature._id,
+          "type": currentSignature.type,
+          "algorithm": currentSignature.algorithm,
+          "label": currentSignature.label,
+          "description": currentSignature.description,
+          "samples": [],
+          "gene_label": currentSignature.gene_label,
+        };
+
+
+
+        signature_scores_old.find({ "name": currentSignature.description })
+            .forEach(function (currentOldScore) {
+          newSignatureScore.samples.push({
+            "patient_label": getPatientLabelFromSampleLabel(currentOldScore.id),
+            "sample_label": currentOldScore.id,
+            "value": currentOldScore.val || currentOldScore.value,
+          });
+        });
+
+        newSignatureScore.samples = newSignatureScore.samples
+            .sort(function (first, second) {
+          return first.value - second.value;
+        });
+
+        CohortSignatures.insert(newSignatureScore, insertCallback);
       });
-    });
-
-    newSignatureScore['sample_values'] = newSignatureScore['sample_values']
-        .sort(function (first, second) {
-      return first.value - second.value;
-    });
-
-    CohortSignatures.insert(newSignatureScore, insertCallback);
-  });
 
   console.log("done generating signature scores");
 };
